4504641d4f764c32c5ce27798471515bc6a20f07
easyzoom <easyzoom@gmail.com>
2021-09-01 23:53:02 +0800

    first

---
+++ stm32f407_JavaScript/STM32F407_JavaScript/rtconfig_preinc.h
@@ -0,0 +1,15 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define CONFIG_DISABLE_ES2015
+#define HAVE_CCONFIG_H
+#define JERRY_ENABLE_ERROR_MESSAGES
+#define JERRY_ENABLE_EXTERNAL_CONTEXT
+#define JERRY_JS_PARSER
+#define RT_USING_NEWLIB
+
+#endif /*RTCONFIG_PREINC_H__*/
